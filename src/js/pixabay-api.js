2f6216dd434881a5dc8fc7f796ad45b6c7b1c9c7
Oksana Ilnytska <okailnytska@gmail.com>
2026-08-02 21:25:01 +0200

    Project 12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,17 +3,20 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '51926469-17331259ac3d6722fcf98d7c3';
 
-export function getImagesByQuery(query) {
-return axios.get(BASE_URL, {
+export async function getImagesByQuery(query, page) {
+const response = await axios.get(BASE_URL, {
 params: {
 key: API_KEY,
 q: query,
 image_type: 'photo',
 orientation: 'horizontal',
+page: page,
+per_page: 15,
 safesearch: true,
 },
 })
-.then(response => response.data);
+console.log(response.data);
+return response.data;
 }
 
 
--- src/main.js
+++ src/main.js
@@ -1,38 +1,61 @@
 
 
 import { getImagesByQuery } from './js/pixabay-api.js';
-import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions.js';
+import { createGallery, clearGallery, showLoader, hideLoader, showLoadMoreButton, hideLoadMoreButton} from './js/render-functions.js';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const searchForm = document.querySelector('.form');
 const searchInput = searchForm.querySelector('input[name="search-text"]');
+const loadMoreBtn = document.querySelector('.load-more');
 
-searchForm.addEventListener('submit', (event) => {
+let page = 1;
+let currentQuery = '';
+let totalPages = 0;
+
+
+
+searchForm.addEventListener('submit', async (event) => {
     event.preventDefault();
-
+    
     const query = searchInput.value.trim();
+    
     if (!query) return;
-  
+    currentQuery = query;
+    page = 1;
+
     clearGallery();
+    hideLoadMoreButton();
     showLoader();
   
-    getImagesByQuery(query)
-      .then(data => {
+    try { const data = await getImagesByQuery(currentQuery, page);
+      
+      
         if (!data.hits || data.hits.length === 0) {
+          
           iziToast.error({  
             message: `Sorry, there are no images matching your search ${query}. Please try again!`,
           position: 'topRight',
           timeout: 5000,
-          //backgroundColor: '#EF4040',
-          //messageColor: '#FAFAFB',
-          
+          backgroundColor: '#EF4040',
+          messageColor: '#FAFAFB', 
           });
           return;
         }
+        totalPages = Math.ceil(data.totalHits / 15);
         createGallery(data.hits);
-      })
-      .catch(error => {
+        if (page >= totalPages) {
+        showLoadMoreButton();
+        
+        iziToast.info({
+          message: "We're sorry, but you've reached the end of search results.",
+          position: 'topright',
+          timeout: 5000,
+        });
+      } else { 
+        showLoadMoreButton();
+      }
+      } catch(error) {
         iziToast.error({ 
           message: error.message,
           position: 'topRight',
@@ -41,11 +64,56 @@ searchForm.addEventListener('submit', (event) => {
           messageColor: '#FAFAFB',
           class: 'error-toast'
         });
-      })
-      .finally(() => {
+      }
+      finally {
         hideLoader();
         searchForm.reset();
-      });
+      }
   });
+
+  loadMoreBtn.addEventListener("click", async () => {
+    showLoader();
+
+    try {
+      page+=1;
+    const data = await getImagesByQuery(currentQuery, page);
+    createGallery(data.hits); 
+    const card = document.querySelector('.gallery.item');
+    if (card) {
+      const cardHeight = card.getBoundingClientRect().height;
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+    }
+
+    if (page >= totalPages) {
+      hideLoadMoreButton();
+
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topright',
+        timeout: 5000,
+      });
+    }
+    }catch(error) {
+      iziToast.error({ 
+        message: error.message,
+        position: 'topRight',
+        timeout: 5000,
+        backgroundColor: '#EF4040',
+        messageColor: '#FAFAFB',
+        class: 'error-toast'
+      });
+    }
+    finally {
+      hideLoader();
+    }
+  });
+
+  
+
+  
 
   
